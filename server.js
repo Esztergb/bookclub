00f224d50a3b4e289502375396adf2b51322cf0a
Eszter Barbuscia <gubiscom@yahoo.com>
2023-07-11 20:23:13 -0700

    new commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const express = require('express');
+// const path = require('path');
+// const express = require('express');
 // const session = require('express-session');
 // const exphbs = require('express-handlebars');
 // const routes = require('./controllers');
@@ -8,8 +8,8 @@ const express = require('express');
 // const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 // const hbs = exphbs.create({ helpers });
@@ -44,11 +44,3 @@ const PORT = process.env.PORT || 3001;
 // sequelize.sync({ force: false }).then(() => {
 //   app.listen(PORT, () => console.log('Now listening'));
 // });
-app.use(express.static(path.join(__dirname, 'public')));
-// Sets up the routes
-app.use(require('./controllers/dish-routes'));
-
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
-});
