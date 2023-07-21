bce52d953582009538ed6d1c60567516c9c24f5c
nporter <nporterwork@gmail.com>
2023-07-20 20:58:21 -0700

    still working on favorites

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
-const { Book } = require('../../models');
+const { Book, UserFavorites } = require('../../models');
 const withAuth = require('../../utils/auth');
+const { Op } = require('sequelize');
+// Fetch books by title
+router.get('/', async (req, res) => {
+  try {
+    const { title } = req.query;
 
+    const books = await Book.findAll({
+      where: {
+        title: {
+          [Op.like]: `%${title}%`,
+        },
+      },
+    });
+
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// Create a new book
 router.post('/', withAuth, async (req, res) => {
   try {
     const newBook = await Book.create({
@@ -15,6 +36,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// Delete a book by its ID
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const bookData = await Book.destroy({
@@ -34,5 +56,42 @@ router.delete('/:id', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Add a book to favorites
+router.post('/favorites', withAuth, async (req, res) => {
+  try {
+    console.log(req.body,req.session.user_id)
+    const newFavorite = await UserFavorites.create({
+      user_id: req.session.user_id,
+      book_id: req.body.book_id,
+    });
+
+    res.status(200).json(newFavorite);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a book from favorites by its ID
+router.delete('/favorites/:id', withAuth, async (req, res) => {
+  try {
+    const favoriteData = await UserFavorites.destroy({
+      where: {
+        user_id: req.session.user_id,
+        book_id: req.params.id,
+      },
+    });
+
+    if (!favoriteData) {
+      res.status(404).json({ message: 'No favorite book found with this id!' });
+      return;
+    }
+
+    res.status(200).json(favoriteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
